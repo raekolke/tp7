c8f94acd8945ab2f7b6f173e3381a9343c68d3b3
Rae Kolke <sheilakolke@gmail.com>
2021-03-19 23:17:17 +0000

    added final recipe

--- scripts.js
+++ scripts.js
@@ -88,7 +88,14 @@ chicken = new Recipe(
   "chicken-directions.html"
 );
 
-
+burger = new Recipe(
+  "Slider-Style Mini Burgers",
+  "https://images.unsplash.com/photo-1456418047667-56bcd35b1a88?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+  "Jake Stephens",
+  "burger-ingredients.html",
+  "burger-equipment.html",
+  "burger-directions.html"
+);
 
 
 
